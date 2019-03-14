0d35eedb7a03e7ff4d7987fa6426b191eb868b3b
Isa Jansson <isa.jansson@cision.com>
2019-03-14 09:47:34 +0100

    renamed some files and made sure all the html templates are configured #NWS-720

--- webpack.config.js
+++ webpack.config.js
@@ -63,14 +63,104 @@ module.exports = {
     new ExtractTextPlugin('style.css'),
     new CleanWebpackPlugin('dist', {}),
     new HtmlWebpackPlugin({
-      inject: false,
+      inject: true,
       template: './src/index.html',
       filename: 'index.html'
     }),
     new HtmlWebpackPlugin({
-      inject: false,
+      inject: true,
       template: './src/insiders.html',
       filename: 'insiders.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/calendar.html',
+      filename: 'calendar.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/calendarEvent.html',
+      filename: 'calendarEvent.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/mediafeed.html',
+      filename: 'mediafeed.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/orderbook.html',
+      filename: 'orderbook.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/ownership.html',
+      filename: 'ownership.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/estimatePackage1.html',
+      filename: 'estimatePackage1.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/estimatePackage2.html',
+      filename: 'estimatePackage2.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/estimatePackage3.html',
+      filename: 'estimatePackage3.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/performance.html',
+      filename: 'performance.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/printedMaterial.html',
+      filename: 'printedMaterial.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/newsfeed.html',
+      filename: 'newsfeed.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/releaseDetail.html',
+      filename: 'releaseDetail.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/shareCalculator.html',
+      filename: 'shareCalculator.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/sharegraphSmall.html',
+      filename: 'sharegraphSmall.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/sharegraph.html',
+      filename: 'sharegraph.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/tickerLarge.html',
+      filename: 'tickerLarge.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/tickerSmall.html',
+      filename: 'tickerSmall.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/trades.html',
+      filename: 'trades.html'
     }),
     new webpack.ProvidePlugin({
       jquery: 'jquery',
